a58682219826770300ac08fd201c9cfff7e02b36
truonggiang <truonggiang76543@gmail.com>
2020-12-17 20:11:01 +0700

    vueneư

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+<!-- //lọc dữ liệu sắp xếp tìm kiếm -->
+    <!-- <div>{{Loc}}</div> -->
+    <compLeft/>
+    <compRight/>
+    <compCreateUnion/>
+    <!-- <CompFilter/> -->
+  </div>
+</template>
+<script>
+import compLeft from './components/compLeft'
+import compRight from './components/compRight'
+import compCreateUnion from './components/compCreateUnion'
+import compFilter from './components/compFilter'
+export default {
+  name: 'App',
+  components: {
+    compLeft,
+    compRight,
+    compCreateUnion,
+    compFilter
+  }
+}
+</script>
+<style scoped>
+#app {
+  margin: 0px;
+  padding: 0px;
+  position: relative;
+  display: flex;
+}
+body {
+  margin: 0px;
+  padding: 0px;
+  position: relative;
+}
+</style>
